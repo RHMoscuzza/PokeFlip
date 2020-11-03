6268f0889329f61dc991ddb6fdf398069a3feaba
Rohan Bhangui <bhanguirohan@gmail.com>
2020-11-02 23:29:47 -0500

    fixed the state for toggling

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 
 function App(){
 	const [data, setData] = useState([]);
-	const [isToggled, setToggle] = useState(false);
+	const [isToggled, setIsToggle] = useState({});
 	const [toggleID, setToggleID] = useState()
 
 	useEffect(() => {
 		const baseUrl = `https://pokeapi.co/api/v2/pokemon?limit=151`;
 
-		async function fetchData() {
+		const fetchData = async () => {
 			const result = await axios
 			.get(baseUrl)
 			.then((res) => {
@@ -20,16 +20,46 @@ function App(){
 				return Promise.all(results.map((res) => axios.get(res.url)))
 			})
 			.then((results) => {
-				setData(results.map((res) => res.data))
+				return results.map((res) => res.data)
 			})
+
+			setData(result);
 		}
 
 		fetchData();
 
+		// I wrote my own fetch using fetch function (fetch is built-in as opposed to axios)
+		//
+		// fetch(baseUrl)
+		// 	.then(response => response.json())
+		// 	.then(({results}) => {
+		// 		// used your technique for returning promises, but instead
+		// 		// i use the function that fetches the additional data
+		// 		// to construct my objects
+		// 		return Promise.all(results.map(item => fetchAdditionalData(item)))
+		// 			.then((values) => {
+		// 				setData(values);
+		// 			})
+		// 	})
 	}, []);
 
-	const toggleTrueFalse = (index) => {
-		setToggle(!isToggled)
+	// used to fetch additional data
+	// const fetchAdditionalData = ({ name, url}) => {
+	// 	return fetch(url)
+	// 		.then(response => response.json())
+	// 		.then(json => {
+	// 			// construct the object to be added to the data arr
+	// 			return { name, url, ...json }
+	// 		});
+	// }
+
+	const toggleTrueFalse = (key) => {
+		return (e) => {
+			// we do this to test if the key exists in the object, which if it does
+			// will result in undefined (falsey value)
+			// as a result we are able to store the individual state of each tile
+			setIsToggle({ ...isToggled, [key]: !isToggled[key] })
+		}
 	}
 
 	return (
@@ -38,15 +68,15 @@ function App(){
 				<h1>PokeFlip</h1>
 			</header>
 			<ul>
-				{data.map((item, index) => (
+				{data && data.map((item, index) => 
 					<li key={item.name} id={index}>
-
 						<h5 className="pokeId" >{item.id}</h5>
 						<h3 className="pokeName">{item.name.replace('-f', ' ♀').replace('-m', ' ♂')}</h3>
-						<> {!isToggled ?
+						{!isToggled[item.name] ? (
 							<div className="flipCard frontCard" >
-								<img src={item.sprites.front_default} alt=""/>
-							</div> :
+								<img src={item.sprites.front_default} alt="" />
+							</div>
+						) : (
 							<div className="flipCard backCard" >
 								<p key={index} className="pokeType">Type:&nbsp;{
 									item.types.map((singleType, i) => {
@@ -54,15 +84,15 @@ function App(){
 									})
 								}
 								</p>
-								<p>Height: {(item.height/10).toFixed(2)}m</p>
-							<p>Weight: {item.weight/10}kg</p>
+								<p>Height: {(item.height / 10).toFixed(2)}m</p>
+								<p>Weight: {item.weight / 10}kg</p>
 								<p>Ability: {item.abilities[0].ability.name}</p>
 								<p>Base Exp: {item.base_experience}</p>
 							</div>
-						}</>
-						<button className='flipButton' onClick={toggleTrueFalse.bind(this, index)}>{!isToggled ? `More Stats` : `Less Stats`}</button>
+						)}
+						<button className='flipButton' onClick={toggleTrueFalse(item.name)}>{!isToggled[item.name] ? `More Stats` : `Less Stats`}</button>
 					</li>
-				))}
+				)}
 			</ul>
 		</div>
 	);
